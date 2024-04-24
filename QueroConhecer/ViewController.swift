d8cd8aac1935c20fb785e916bf1f5aac1f3361b9
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-04-23 23:32:17 -0300

    Initial Commit

---
+++ QueroConhecer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QueroConhecer
+//
+//  Created by Eduardo Frederico on 23/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
